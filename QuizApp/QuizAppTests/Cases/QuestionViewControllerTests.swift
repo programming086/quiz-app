7691008a667f8cf28552695932d2ff15aadf3cde
Roman Brovko <brovko-roman@ya.ru>
2023-04-05 20:34:32 +0300

    [T] add tests renderOptions in QuestionViewController

--- QuizApp/QuizAppTests/Cases/QuestionViewControllerTests.swift
+++ QuizApp/QuizAppTests/Cases/QuestionViewControllerTests.swift
@@ -12,11 +12,28 @@ import XCTest
 
 final class QuestionViewControllerTests: XCTestCase {
 
-    func test_viewDidLoad_renderQuestionHeaderTest() throws {
-        let sut = QuestionViewController(question: "Q1")
+    func test_viewDidLoad_renderQuestionHeaderText() throws {
+        let sut = QuestionViewController(question: "Q1", options: [])
         
         _ = sut.view
         
         XCTAssertEqual(sut.headerLabel.text, "Q1")
     }
+    
+    func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
+        let sut = QuestionViewController(question: "Q1", options: [])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    func test_viewDidLoad_withOneOption_rendersOneOption() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+    }
+
 }
