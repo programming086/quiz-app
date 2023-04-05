ae3424cab3ba2449fa3d9dd5d9ed3d6824f4990a
Roman Brovko <brovko-roman@ya.ru>
2023-04-05 20:34:32 +0300

    [T] add tests renderOptions in QuestionViewController

--- QuizApp/QuizApp/QuestionViewController.swift
+++ QuizApp/QuizApp/QuestionViewController.swift
@@ -11,12 +11,15 @@ import UIKit
 class QuestionViewController: UIViewController {
     
     @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
-    private var question: String!
+    private var question: String = ""
+    private var options: [String] = []
     
-    convenience init(question: String) {
+    convenience init(question: String, options: [String]) {
         self.init()
         self.question = question
+        self.options = options
     }
 
     override func viewDidLoad() {
@@ -26,3 +29,16 @@ class QuestionViewController: UIViewController {
     }
 }
 
+extension QuestionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = options[indexPath.row]
+        
+        return cell
+    }
+
+}
